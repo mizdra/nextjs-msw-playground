6bc17ea98e4cb45e76f37accb96fdfb52970c3bc
mizdra <pp.mizdra@gmail.com>
2023-07-14 00:53:19 +0900

    msw/node では handler を変更してもライブリロードが反映されない問題を修正

--- lib/msw/handler.ts
+++ lib/msw/handler.ts
@@ -4,7 +4,7 @@ export const handlers = [
   graphql.query('HomeQuery', () => {
     return HttpResponse.json({
       data: {
-        hello: 'Hello world!',
+        hello: 'Hello World!',
       }
     })
   })
--- lib/msw/server.ts
+++ lib/msw/server.ts
@@ -1,4 +1,15 @@
 import { setupServer } from 'msw/node'
 import { handlers } from './handler'
 
-export const server = setupServer(...handlers)
+// msw/node には handler の変更がライブリロードで反映されず、古い handler から
+// レスポンスが返され続ける問題がある。
+// ref: https://github.com/mswjs/msw/issues/1389
+// これはライブリロードされた時に、古いバージョンの server が listen し続けているため引き起こされているものと思われる。
+// そこで、ライブリロードされた時に古い server を close することで、この問題を回避する。
+
+if ('server' in globalThis) {
+  (globalThis as any).server.close();
+}
+
+export const server = setupServer(...handlers);
+(globalThis as any).server = server;
